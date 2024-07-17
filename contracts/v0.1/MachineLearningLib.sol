bd1bf4d3eb924acd75509fcae27c2c7aa2e7526c
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-07-17 18:12:58 +0530

    Add: Cid data extraction

--- contracts/v0.1/MachineLearningLib.sol
+++ contracts/v0.1/MachineLearningLib.sol
@@ -5,12 +5,15 @@ import { CommonTypes } from "filecoin-solidity-api/contracts/v0.8/types/CommonTy
 import "filecoin-solidity-api/contracts/v0.8/utils/Actor.sol";
 import "filecoin-solidity-api/contracts/v0.8/utils/CborDecode.sol";
 import "solidity-cborutils/contracts/CBOR.sol";
+import "filecoin-solidity-api/contracts/v0.8/cbor/FilecoinCbor.sol";
 
 // Library for Machine Learning functionalities
 library MachineLearningLib {
   using CBOR for CBOR.CBORBuffer;
+  using FilecoinCBOR for *;
 
   // Function to train a linear regression model
+  // Solver: QR Decomposition
   function trainLinearRegression(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -43,6 +46,7 @@ library MachineLearningLib {
   }
 
   // Function to train a logistic regression model
+  // Solver: Limited-memory Broyden–Fletcher–Goldfarb–Shanno (LBFGS) method
   function trainLogisticRegression(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -75,6 +79,8 @@ library MachineLearningLib {
   }
 
   // Function to train a K-Nearest Neighbors (KNN) regression model
+  // Search algorithm: CoverTree
+  // K: 3
   function trainKNNRegression(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -107,6 +113,8 @@ library MachineLearningLib {
   }
 
   // Function to train a KNN classification model
+  // Search algorithm: CoverTree
+  // K: 3
   function trainKNNClassification(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -203,6 +211,7 @@ library MachineLearningLib {
   }
 
   // Function to train a Random Forest regression model
+  // Number of trees: 100
   function trainRandomForestRegression(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -235,6 +244,7 @@ library MachineLearningLib {
   }
 
   // Function to train a Random Forest classification model
+  // Number of trees: 100
   function trainRandomForestClassification(int64[][] memory data, int64[] memory labels) internal returns (bytes memory) {
     // Serialize training data and labels
     bytes memory params = _serializeTrainingInput(data, labels);
@@ -263,6 +273,23 @@ library MachineLearningLib {
 
     // Deserialize and return the prediction results
     int64[] memory result = _deserializePredictionResult(return_value);
+    return result;
+  }
+
+  // Function to extract data from Cid
+  function cidDataExtractionSyscall(bytes memory cid) public returns (int64[][] memory) {
+    // Convert bytes cid into Cid format and serialize
+    CommonTypes.Cid memory dataCid = CommonTypes.Cid(cid);
+    bytes memory params = _serializeCidInput(dataCid);
+
+    // Call the actor's cid extraction function
+    (int256 ret_code, bytes memory return_value) =
+          Actor.callByID(CommonTypes.FilActorId.wrap(49), 822806271, Misc.CBOR_CODEC, params, 0, false);
+    
+    require(ret_code == 0, "Machine learning actor failed");
+
+    // Deserialize and return the result
+    int64[][] memory result = _deserializeCidData(return_value);
     return result;
   }
 
@@ -316,6 +343,16 @@ library MachineLearningLib {
     return buf.data();
   }
 
+  // Helper function to serialize cid input
+  function _serializeCidInput(CommonTypes.Cid memory dataCid) private pure returns (bytes memory) {
+      // have 256 as initial capacity
+      CBOR.CBORBuffer memory buf = CBOR.create(256);
+      buf.startFixedArray(1);
+      buf.writeCid(dataCid.data);
+
+      return buf.data();
+    }
+
   // Helper function to deserialize prediction result
   function _deserializePredictionResult(bytes memory actor_result) private pure returns (int64[] memory) {
     (uint256 len, uint256 byteIdx) = CBORDecoder.readFixedArray(actor_result, 0);
@@ -341,6 +378,26 @@ library MachineLearningLib {
 
       result[i] = item;
       byteIdx = idx;
+    }
+
+    return result;
+  }
+
+  function _deserializeCidData(bytes memory _cidData) private pure returns (int64[][] memory) {
+    (uint256 len, uint256 byteIdx) = CBORDecoder.readFixedArray(_cidData, 0);
+
+    int64[][] memory result = new int64[][](len);
+    for (uint256 i = 0; i < len; i++) {
+      (uint256 rowLen, uint256 rowIdx) = CBORDecoder.readFixedArray(_cidData, byteIdx);
+      result[i] = new int64[](rowLen);
+      for (uint256 j = 0; j < rowLen; j++) {
+        (int64 item, uint256 idx) = CBORDecoder.readInt64(_cidData, rowIdx);
+
+          result[i][j] = item;
+          rowIdx = idx;
+      }
+
+      byteIdx = rowIdx;
     }
 
     return result;
